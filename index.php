593a6f08c6ff4a6712d8af6eae8fe44340b5871f
Juan Encabo <encabojuan@gmail.com>
2024-09-09 15:30:37 -0300

    bd

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 
 
-echo " anda en el vps";
+
+$host = '85.209.92.152';
+$db = 'durcor_database';
+$user = 'ak4n1';
+$pass = 'F4^k9&l2zW!7v@1D';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa a la base de datos.";
+} catch (PDOException $e) {
+    echo "Error en la conexión: " . $e->getMessage();
+}
+?>
